529c01da39d5ce736936082119de07a25d2c83a7
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 00:06:54 +0100

    Failing test for #11

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,6 +8,7 @@ class AuthorsController < ApplicationController
   end
 
   def new
+
   end
 
   def create
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,15 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should show an error when an input field wasn't filled out" do
+    visit new_author_path
+
+    fill_in 'author[first_name]', with: "Alan"
+    fill_in 'author[homepage]', with: "http://wikipedia.org/Alan_Turing"
+
+    click_on 'submit'
+
+    expect(page).to have_text('Expected to find text matching /error/i in "all authors\nnew author\nfirst name last name homepage alan turing http://wikipedia.org/alan_turing view"')
+  end
 end
