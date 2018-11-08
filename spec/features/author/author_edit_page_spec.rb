f28cba7888ea1eb994779f49d968d9b70b77f82d
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 00:43:00 +0100

    Failing test for #13

--- spec/features/author/author_edit_page_spec.rb
+++ spec/features/author/author_edit_page_spec.rb
@@ -8,4 +8,15 @@ describe "Author edit page", type: :feature do
     visit edit_author_path(@author)
     expect(page).not_to have_text("The action 'edit' could not be found for AuthorsController")
   end
+
+  it "should show no error when changing the author's first name" do
+    @author = FactoryBot.create :author
+    visit edit_author_path(@author)
+
+    fill_in 'author[first_name]', with: "Alan Mathison"
+
+    click_on 'submit'
+
+    expect(page).not_to have_text('Unable to find field "author[first_name]"')
+  end
 end
