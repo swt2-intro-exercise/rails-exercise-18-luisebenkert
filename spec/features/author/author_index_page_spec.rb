b6098b23295ccb186c5bbf66a57c87d843fc5e93
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-08 23:31:08 +0100

    Failing test for #8

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
+include AuthorsHelper
 
 describe "Author index page", type: :feature do
 
   it "should render without error" do
     visit authors_path
     expect(page).not_to have_text("The action 'index' could not be found for AuthorsController")
+  end
+
+  it "should display author's name" do
+    @alan = FactoryBot.create :author
+    name = concat_strings(@alan.first_name, @alan.last_name)
+    visit authors_path
+    expect(page).to have_text(name)
   end
 end
