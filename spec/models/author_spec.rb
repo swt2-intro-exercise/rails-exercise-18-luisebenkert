6170dc0af0a87722d5766ee01482142f69aed461
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-08 21:24:50 +0100

    Failing test for #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,13 +1,18 @@
 describe "New author page", :type => :model do
 
-  it "should create an instance of Author when a new Author is created" do
-    first_name = 'Alan'
-    last_name = 'Turing'
-    homepage = 'http://wikipedia.org/Alan_Turing'
+  first_name = 'Alan'
+  last_name = 'Turing'
+  homepage = 'http://wikipedia.org/Alan_Turing'
 
+  it "should create an instance of Author when a new Author is created" do
     author = Author.create(first_name: first_name, last_name: last_name, homepage: homepage)
     expect(author.first_name).to eq(first_name)
     expect(author.last_name).to eq(last_name)
     expect(author.homepage).to eq(homepage)
+  end
+
+  it "should return the full name on function call name()" do
+    author = Author.create(first_name: first_name, last_name: last_name, homepage: homepage)
+    expect(author.name()).to eq(first_name + ' ' + last_name)
   end
 end
