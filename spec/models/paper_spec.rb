68175dfc92d237c632e39c7cdd47fe2171608761
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 02:53:33 +0100

    Failing test for #22

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -4,6 +4,6 @@ describe "New paper page", :type => :model do
 
   it "should have an empty list of authors" do
     @paper = FactoryBot.create :paper
-    expect(@paper.authors).to eq({})
+    expect(@paper.authors).to eq([])
   end
 end
