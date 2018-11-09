e1bbd37cd7b9ac2a6a2d6620bd873379b761ca3d
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 02:00:34 +0100

    Failing test for #17

--- spec/features/papers/new_paper_spec.rb
+++ spec/features/papers/new_paper_spec.rb
@@ -6,4 +6,11 @@ describe "New Paper page", type: :feature do
     visit new_paper_path
   end
 
+  it "should not validate if title is missing" do
+    visit new_paper_path
+
+    paper = Paper.new(venue: "mind 49: 433-460", year: 1950)
+
+    expect(paper).to_not be_valid
+  end
 end
