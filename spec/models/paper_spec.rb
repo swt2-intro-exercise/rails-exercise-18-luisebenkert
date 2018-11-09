7e6ff51b72e000280cc8d150c58cd8edb44c5b28
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 02:38:14 +0100

    Failing test for #21

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,2 +1,9 @@
-require 'rails_helper'
+include AuthorsHelper
 
+describe "New paper page", :type => :model do
+
+  it "should have an empty list of authors" do
+    @paper = FactoryBot.create :paper
+    expect(@paper.authors).to eq({})
+  end
+end
