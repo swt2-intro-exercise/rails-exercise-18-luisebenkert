f01ae6811190d23caa2beedd91edfed69d153fd0
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2018-11-09 13:48:15 +0100

    Failing test for #26

---
+++ spec/features/papers/paper_edit_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Paper edit page", type: :feature do
+
+  it "should have a single multiple select box" do
+    @paper = FactoryBot.create :paper
+    visit paper_path(@paper)
+    expect(page).to have_css("select[multiple]", count:1)
+  end
+
+end
